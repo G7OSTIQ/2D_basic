240a79d593c9a9fca33e0f5e026332cbfdbee147
G7OSTIQ <106563737+G7OSTIQ@users.noreply.github.com>
2026-04-16 14:55:22 +0200

    player animations
    In this commit animation was added onto the player

--- Assets/script/player_controls.cs
+++ Assets/script/player_controls.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Windows;
-[RequireComponent(typeof(Rigidbody2D),typeof(SpriteRenderer))]// what this does will auto add a rigibody in 2D
+[RequireComponent(typeof(Rigidbody2D),typeof(SpriteRenderer),typeof(Animator))]// what this does will auto add a rigibody in 2D
 
 public class player_controls : MonoBehaviour
 {
@@ -13,16 +13,35 @@ public class player_controls : MonoBehaviour
 
     public Transform groundCheck;
 
+    private int jumpleft = 2;
+
 
     private Rigidbody2D myrigitbody2D;
     private SpriteRenderer mespriterender;
+    private Animator myanimator;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-         myrigitbody2D = GetComponent<Rigidbody2D>();
+        myrigitbody2D = GetComponent<Rigidbody2D>();
         mespriterender = GetComponent<SpriteRenderer>();
+        myanimator = GetComponent<Animator>();
+    }
+
+
+    private void FixedUpdate()
+    {
+
+
+
+        float velocityX = Mathf.Abs(myrigitbody2D.linearVelocityX);
+    
+        myanimator.SetFloat("Velocity", velocityX);
+        myanimator.SetFloat("Falling_Velocity", myrigitbody2D.linearVelocityY);
+        myanimator.SetBool("Jumping", isGrouned());
+        myanimator.SetInteger("Jump_Left", jumpleft);
+       
     }
 
     private void OnMove(InputValue value)
@@ -41,13 +60,21 @@ public class player_controls : MonoBehaviour
 
     private void OnJump(InputValue value)
     {
-        if (value.isPressed && isGrouned())
+        if (isGrouned())
+        {
+            jumpleft = 2;
+        }
+
+        if (value.isPressed && (isGrouned() || jumpleft > 0))
         {
             myrigitbody2D.linearVelocityY = jump_force;
+            jumpleft--;
         }
 
 
     }
+
+
 
     //will check if ground is collided
     private bool isGrouned()
